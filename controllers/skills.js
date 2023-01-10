78c40c726234ceccb40f8d62ec5ce993bdcb6d38
CharlieMickey <charles.f.mickey@gmail.com>
2023-01-10 10:57:05 -0600

    add index update and delete

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,5 +1,17 @@
 import {Skill} from "../models/skill.js";
 
+
+function index(req,res){
+Skill.find({})
+.then(skills =>{
+  res.render('skills/index')
+})
+.catch(err =>{
+  console.log(err)
+  res.redirect('/')
+})
+}
+
 function create(req,res){
 Skill.find({})
 req.body.confident = Boolean(req.body.confident)
@@ -7,15 +19,69 @@ Skill.create(req.body)
 .then(skill=>{
   res.redirect('/skills')
 })
+.catch(err=>{
+  console.log(err)
+  res.redirect('/')
+})
 }
 
 function newSkill(req,res){
   res.redirect('/skills/new')
+}
 
+function show(req, res){
+  Skill.findById(req.params.id)
+  .then(skill=>{
+    res.render('skills/show',{
+      skill:skill,
+    })
+  })
+  .catch(err=>{
+    console.log(err)
+    res.redirect('/')
+  })
+}
+function edit(req,res){
+  Skill.findById(req.params.id)
+  .then(skill=>{
+    res.render(`skills/${req.params.id}/edit`,{
+
+    })
+  })
+  .catch(error=>{
+    console.log('error')
+    res.redirect('/')
+  })
+
+}
+function update(req,res){
+  req.body.confident = !!req.body.confident
+  Skill.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  .then(skill=>{
+    res.redirect('/skills')
+  })
+  .catch(error=>{
+    console.log('error')
+    res.redirect('/')
+  })
+}
+function deleteSkill(req,res){
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill =>{
+    res.redirect('/skills')
+  })
+  .catch(error=>{
+    console.log('error')
+    res.redirect('/')
+  })
 }
 
 export{
   create,
   newSkill as new,
-  
+  index,
+  show,
+  edit,
+  update,
+  deleteSkill as delete,
 }
--- routes/skills.js
+++ routes/skills.js
@@ -2,9 +2,15 @@ import { Router } from 'express'
 import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
+router.get('/', skillsCtrl.index)
+
 router.get('/new', skillsCtrl.new)
 
-router.post('/', skillsCtrl.create)
+router.post('/skills', skillsCtrl.create)
+
+router.put('/:id/edit', skillsCtrl.update)
+
+router.delete('/:id', skillsCtrl.delete)
 
 export {
   router
